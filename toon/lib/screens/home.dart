3946a771070e46c50354e76ba985b6a0aa408633
yongjunhan <hanyongjun.dev@gmail.com>
2023-03-14 19:50:22 +0900

    cover IMG,ListView

--- toon/lib/screens/home.dart
+++ toon/lib/screens/home.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     print(webtoons);
     return Scaffold(
-      backgroundColor: const Color.fromARGB(221, 21, 21, 21),
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 1,
         foregroundColor: Colors.white,
@@ -28,26 +28,69 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                // print(index);
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title,
-                    style: const TextStyle(
-                      color: Colors.white,
-                    ));
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 20,
-              ),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 40,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                )
+              ],
             );
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        // print(index);
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 20,
+                        offset: const Offset(0, 0),
+                        color: Colors.black.withOpacity(0.3))
+                  ]),
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                  color: Color.fromARGB(255, 114, 114, 114),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
